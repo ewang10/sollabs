ac65d0749be58e88e54b5cc71c0ee29331fcd249
eric <ericwang27759@gmail.com>
2022-01-30 20:14:10 -0800

    added shadow grow animation to project page

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -10,74 +10,78 @@ const Project = () => {
     return (
         <Box px={{ base: '8', md: '40' }} py='8' maxWidth='1200px' marginX='auto'>
             <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }} gap={{ base: '10', md: '20' }}>
-                <Box
-                    {...commonProps}
-                    background="url('/images/BuddyBotsProfile.png') no-repeat"
-                    backgroundSize="contain"
-                    backgroundPosition="bottom"
-                >
+                <div className="project">
                     <Link to="/projects/buddy_bots">
+                        <Box
+                            {...commonProps}
+                            background="url('/images/BuddyBotsProfile.png') no-repeat"
+                            backgroundSize="contain"
+                            backgroundPosition="bottom"
+                        >
+                            <Text
+                                fontWeight="bold"
+                                fontSize="3xl"
+                                align="center"
+                                padding={{ base: "20px", md: "50px" }}
+                                backgroundColor="blue.100"
+                                color="white"
+                            >
+                                Buddy Bots
+                            </Text>
+                        </Box>
+                    </Link>
+                </div>
+                <div className="project">
+                    <Box
+                        {...commonProps}
+                        background="url('/images/Coming-Soon.png') no-repeat"
+                        backgroundSize="contain"
+                        backgroundPosition="bottom"
+                    >
                         <Text
                             fontWeight="bold"
                             fontSize="3xl"
                             align="center"
                             padding={{ base: "20px", md: "50px" }}
-                            backgroundColor="blue.100"
-                            color="white"
                         >
-                            Buddy Bots
+                            Announcing soon...
                         </Text>
-                    </Link>
-                </Box>
-                <Box
-                    {...commonProps}
-                    background="url('/images/Coming-Soon.png') no-repeat"
-                    backgroundSize="contain"
-                    backgroundPosition="bottom"
-                >
-                    <Text
-                        fontWeight="bold"
-                        fontSize="3xl"
-                        align="center"
-                        padding={{ base: "20px", md: "50px" }}
+                    </Box>
+                </div>
+                <div className="project">
+                    <Box
+                        {...commonProps}
+                        background="url('/images/Coming-Soon.png') no-repeat"
+                        backgroundSize="contain"
+                        backgroundPosition="bottom"
                     >
-                        Announcing soon...
-                    </Text>
-                </Box>
-                <Box
-                    {...commonProps}
-                    background="url('/images/Coming-Soon.png') no-repeat"
-                    backgroundSize="contain"
-                    backgroundPosition="bottom"
-                >
-                    {/* <Link to="/projects/solman"> */}
-                    <Text
-                        fontWeight="bold"
-                        fontSize="3xl"
-                        align="center"
-                        padding={{ base: "20px", md: "50px" }}
+                        <Text
+                            fontWeight="bold"
+                            fontSize="3xl"
+                            align="center"
+                            padding={{ base: "20px", md: "50px" }}
+                        >
+                            Announcing soon...
+                        </Text>
+                    </Box>
+                </div>
+                <div className="project">
+                    <Box
+                        {...commonProps}
+                        background="url('/images/Coming-Soon.png') no-repeat"
+                        backgroundSize="contain"
+                        backgroundPosition="bottom"
                     >
-                        Announcing soon...
-                    </Text>
-                    {/* </Link> */}
-                </Box>
-                <Box
-                    {...commonProps}
-                    background="url('/images/Coming-Soon.png') no-repeat"
-                    backgroundSize="contain"
-                    backgroundPosition="bottom"
-                >
-                    {/* <Link to="/projects/solman"> */}
-                    <Text
-                        fontWeight="bold"
-                        fontSize="3xl"
-                        align="center"
-                        padding={{ base: "20px", md: "50px" }}
-                    >
-                        Announcing soon...
-                    </Text>
-                    {/* </Link> */}
-                </Box>
+                        <Text
+                            fontWeight="bold"
+                            fontSize="3xl"
+                            align="center"
+                            padding={{ base: "20px", md: "50px" }}
+                        >
+                            Announcing soon...
+                        </Text>
+                    </Box>
+                </div>
             </Grid >
         </Box >
     )
